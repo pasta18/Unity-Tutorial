b3bf41ff1e5eb7c580c534ee0de32915be7d53a2
pasta18 <lightsworn449black@gmal.com>
2015-08-18 20:22:31 +0900

    Correction
    Change to fit the screen size

--- Assets/Scripts/BackGround.cs
+++ Assets/Scripts/BackGround.cs
@@ -5,7 +5,15 @@ public class BackGround : MonoBehaviour {
 
     public float speed = 0.1f;
 
-	
+    void Start()
+    {
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        Vector2 scale = max * 2;
+
+        transform.localScale = scale;
+    }
+
 	// Update is called once per frame
 	void Update () {
         float y = Mathf.Repeat(Time.time * speed, 1);
--- Assets/Scripts/DestroyArea.cs
+++ Assets/Scripts/DestroyArea.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class DestroyArea : MonoBehaviour {
 
+    void Start()
+    {
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        Vector2 size = max * 2;
+
+        GetComponent<BoxCollider2D>().size = size;
+    }
+
     void OnTriggerExit2D(Collider2D c)
     {
         Destroy(c.gameObject);
